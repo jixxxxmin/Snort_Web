ed9fec9f98e075db704087459d91038bc8bd8d39
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-08 15:07:36 +0900

    n

--- html/login/js/login.js
+++ html/login/js/login.js
@@ -1,23 +1,22 @@
 
-document.querySelector("form").addEventListener("submit", function(e) {
-  const id = document.getElementById("id");
-  const pw = document.getElementById("password");
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    const id = document.getElementById("id").value.trim();
+    const pw = document.getElementById("password").value.trim();
+    const errorBox = document.getElementById("error-msg");
 
-  if (!id.value.trim()) {
-    e.preventDefault();
-    id.setCustomValidity("아이디를 입력하세요.");
-    id.reportValidity();
-    return;
-  } else {
-    id.setCustomValidity("");
-  }
+    if (!id) {
+        e.preventDefault();
+        errorBox.textContent = "아이디를 입력하세요.";
+        errorBox.style.display = "block";
+        return;
+    }
 
-  if (!pw.value.trim()) {
-    e.preventDefault();
-    pw.setCustomValidity("비밀번호를 입력하세요.");
-    pw.reportValidity();
-    return;
-  } else {
-    pw.setCustomValidity("");
-  }
+    if (!pw) {
+        e.preventDefault();
+        errorBox.textContent = "비밀번호를 입력하세요.";
+        errorBox.style.display = "block";
+        return;
+    }
+
+    errorBox.style.display = "none";
 });

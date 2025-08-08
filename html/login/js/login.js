937dd05fc23da247615cfd641f6d2694ed6e18a9
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-08 15:03:37 +0900

    new

---
+++ html/login/js/login.js
@@ -0,0 +1,23 @@
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  const id = document.getElementById("id");
+  const pw = document.getElementById("password");
+
+  if (!id.value.trim()) {
+    e.preventDefault();
+    id.setCustomValidity("아이디를 입력하세요.");
+    id.reportValidity();
+    return;
+  } else {
+    id.setCustomValidity("");
+  }
+
+  if (!pw.value.trim()) {
+    e.preventDefault();
+    pw.setCustomValidity("비밀번호를 입력하세요.");
+    pw.reportValidity();
+    return;
+  } else {
+    pw.setCustomValidity("");
+  }
+});

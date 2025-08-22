122253e31326e0661750f768942caaf57a7790d3
Min <112598226+jixxxxmin@users.noreply.github.com>
2025-08-22 11:46:01 +0900

    Update submenuEvent.js

--- html/adminSubmenu/js/submenuEvent.js
+++ html/adminSubmenu/js/submenuEvent.js
@@ -143,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 body: formData.toString()
             });
 
-            if (response.status === 200) {
+            if (response.status === 201) {
                 window.showMessage('서브메뉴가 성공적으로 삭제되었습니다.', true);
                 deleteForm.reset();
                 fetchSubmenus();
